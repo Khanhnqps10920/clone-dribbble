1eef140db735835423deaaa77727f38af08c71c0
khanh <khanhnqps10920@fpt.edu.vn>
2020-06-11 18:07:55 +0700

    add new package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
 
+// scss
+import "./assets/scss/main.scss";
+
+// lib
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+
+// component
+import Header from "./components/common/header/Header";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Header />
+    </Router>
+  );
 }
 
 export default App;
--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
+// style
+import "./header.styles.scss";
+
+// lib
+// component
+
 const Header = (props) => {
-  return <div></div>;
+  const [width, setWitdh] = useState(window.innerWidth);
+
+  return (
+    <header className="header">
+      {width}
+      <button
+        onClick={() => {
+          setWitdh(width + 1);
+        }}
+      >
+        Click
+      </button>
+    </header>
+  );
 };
 
 Header.propTypes = {};
